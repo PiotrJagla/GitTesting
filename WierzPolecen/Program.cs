109f2077ae422353c20e251eb7d8b5bef80f8e5a
PiotrJagielny <piotrek2002.09@gmail.com>
2023-01-06 20:12:17 +0100

    komit po zmienie nazwy projektu

--- WierzPolecen/Program.cs
+++ WierzPolecen/Program.cs
@@ -1,4 +1,5 @@
 ﻿
 Console.WriteLine("Hello, World!");
 Console.WriteLine("To zostalo dodane");
-
+Console.WriteLine("To jest wersja sprzed zmiany nazwy projektu gdzie nazywa się WierzPolecen");
+Console.WriteLine("To jest wersje po zmianie nazwy projekty gdzie nazwa teraz jest Polecenia");
