f3c6430d47c378afab70aa162a71eda6ea132eaa
PiotrJagielny <piotrek2002.09@gmail.com>
2023-01-07 08:42:51 +0100

    nowa klasa

---
+++ WierzPolecen/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polecenia
+{
+    public class Book
+    {
+        int number;
+
+        public Book()
+        {
+            number = -1;
+        }
+    }
+}
