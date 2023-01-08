209226a332bad44c5bde6a29c3ed69f1a471f831
PiotrJagielny <piotrek2002.09@gmail.com>
2023-01-08 16:23:22 +0100

    gfdsg

---
+++ QuickDemos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickDemos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
