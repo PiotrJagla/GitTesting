f46c36fa3806c37a603076ab2e1c1b8d752dc806
PiotrJagielny <piotrek2002.09@gmail.com>
2023-01-08 16:25:03 +0100

    monopoly cell strategy pattern demo

---
+++ QuickDemos/MonopolyCellsDemo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickDemos;
+
+public interface CellBuyingable
+{
+    BuyableCellBahaviour BayingLogic();
+}
+
+public class CellThatCanBeBought : CellBuyingable
+{
+    private BuyableCellBahaviour cellBahaviour;
+
+    public CellThatCanBeBought(int BuyCost)
+    {
+        cellBahaviour = new BuyableCell(BuyCost);
+    }
+
+    public BuyableCellBahaviour BayingLogic()
+    {
+        return cellBahaviour;
+    }
+}
+
+
+public interface BuyableCellBahaviour
+{
+    void SetCost(int BuyCost);
+    int GetCost();
+}
+
+public class BuyableCell : BuyableCellBahaviour
+{
+    private int BuyCost;
+
+    public BuyableCell(int BuyCost)
+    {
+        this.BuyCost = BuyCost;
+    }
+    public int GetCost()
+    {
+        return BuyCost;
+    }
+
+    public void SetCost(int BuyCost)
+    {
+        this.BuyCost = BuyCost;
+    }
+}
+
+public class NoBuyableCell : BuyableCellBahaviour
+{
+    public int GetCost()
+    {
+        return 0;
+    }
+
+    public void SetCost(int BuyCost)
+    {
+
+    }
+}
+
--- QuickDemos/Program.cs
+++ QuickDemos/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            CellBuyingable cell = new CellThatCanBeBought(40);
+            cell.BayingLogic().SetCost(146);
+
+            Console.WriteLine(cell.BayingLogic().GetCost());
         }
     }
 }
