692d69e7f21a3d1c3eb8e275ff22a3daa7d50288
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-24 09:57:02 +0200

    delete old projects, add new project

--- WierzPolecen/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("To zostalo dodane");
-Console.WriteLine("To jest wersja sprzed zmiany nazwy projektu gdzie nazywa się WierzPolecen");
-Console.WriteLine("To jest wersje po zmianie nazwy projekty gdzie nazwa teraz jest Polecenia");
